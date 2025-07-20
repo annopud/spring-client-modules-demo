7a37d9fba8e09bdbe99888aec8c4023675512298
annop.u <annop.u@extend-os.com>
2025-07-21 01:00:08 +0700

    Try using Zipkin.

---
+++ first-spring-clients/src/test/java/dev/annopud/first_spring_clients/SpringClientsApplicationTest.java
@@ -0,0 +1,17 @@
+package dev.annopud.first_spring_clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringClientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // This test is used to check if the Spring application context loads successfully.
+        // If there are any issues with the configuration or beans, this test will fail.
+        // No additional assertions are needed here.
+    }
+
+}
